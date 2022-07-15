c69b237e297d15a194284c89df105b0b25934305
Daria <daria.kiseleva@mail.mcgill.ca>
2022-07-14 20:36:31 -0400

    Experimenting more

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ const req_data = {
     "api_key": "z2FzoyGx4LCYPSz9g5iscw",
     "details": [
         {
-            "first_name": "Tim",
-            "last_name": "Zheng",
-            "domain": "apollo.io",
-            "email": "tim@apollo.io",
-            "organization_name": "Apollo"
+            "first_name": "Marcel",
+            "last_name": "Moeller",
+            "organization_name": "Dow"
         },
         {
-            "first_name": "Roy",
-            "last_name": "Chung",
-            "email": "roy@apollo.io",
-            "organization_name": "Apollo"
+            "first_name": "Lars",
+            "last_name": "Domogalla",
+            "organization_name": "Dow Olefinverbund GmbH"
+        },
+        {
+            "first_name": "Lars",
+            "last_name": "Domogalla",
+            "organization_name": "Dow"
         }
     ]
 }
 
 axios
 .post("https://api.apollo.io/api/v1/people/bulk_match", req_data)
-.then(res => console.log(res.data.unique_enriched_records))
+.then(res => console.log(res.data.matches))
